7c694d2c4a234574605628c5c29a120a7df3b610
Martin Miller <miller7@illinois.edu>
2014-06-16 15:56:36 -0500

    get quaternion method

--- src/Sensors.cpp
+++ src/Sensors.cpp
@@ -12,7 +12,7 @@ Sensors::update ( )
 {
     altitude = get_altitude( i );
     acceleration = get_acceleration( i );
-    //quaternion = get_quaternion( i );
+    quaternion = get_quaternion( i );
     //dt = get_dt( i );
     //angular_velocity = get_angular_velocity( i );
     ++i;
@@ -37,4 +37,15 @@ Sensors::get_acceleration ( int i )
     }
     return a;
 }		/* -----  end of method Sensors::get_acceleration  ----- */
+
+    cv::Vec4d
+Sensors::get_quaternion ( int i )
+{
+    cv::Vec4d q;
+    for( int j=0; j<4; ++j )
+    {
+        q[j] = quaternionHist.at<double>(j, i);
+    }
+    return q ;
+}		/* -----  end of method Sensors::get_quaternion  ----- */
 
