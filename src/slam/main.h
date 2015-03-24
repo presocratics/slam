bab9d6135008564061397a3466a8ad365d574af3
Martin Miller <miller7@illinois.edu>
2015-03-24 13:27:35 -0500

    Add measurement steps, modify jacobian for corrimu

--- src/slam/main.h
+++ src/slam/main.h
@@ -17,7 +17,7 @@
 #include "Sensors.hpp"
 //#include "feature.h"
 #include "states.h"
-//#include "view.hpp"
+#include "view.hpp"
 //#include "imagesensor.hpp"
 #include "Sensor.hpp"
 
@@ -29,5 +29,12 @@ void blockAssign ( cv::Mat dst, cv::Mat block, cv::Point tl );
 void jacobianMotionModel( const States& mu, const Sensors& sense, Mat& F_out, double dt );
 void initG ( cv::Mat& G, int nf, double dt );
 void initQ ( cv::Mat& Q, int nf, double Q0, double dt );
+void initR ( cv::Mat& R, double R0, std::vector<int> refFlag );
+void resizeP ( cv::Mat& P, int nf );
+void calcP ( cv::Mat& P, const cv::Mat& F, const cv::Mat& G, const cv::Mat& Q );
+void calcK ( cv::Mat& K, const cv::Mat& H, const cv::Mat& P, const cv::Mat& R );
+void measurementModel( const cv::Vec3d& old_pos, double alt, const Quaternion& qbw,
+        View& meas, View& hmu, Mat& H, States& mu );
+void updateP ( cv::Mat& P, const cv::Mat& K, const cv::Mat& H );
 
 #endif
